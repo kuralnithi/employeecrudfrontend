16139f26dd90c98e972370b3a7ac210ca2adf853
kuralnithi <kuralnithi1999@gmail.com>
2024-02-29 12:53:04 +0530

    render upd

--- src/Components/userlogin/ResetPasswordpage.jsx
+++ src/Components/userlogin/ResetPasswordpage.jsx
@@ -97,7 +97,7 @@ function ResetPasswordpage(props) {
         }
 
 
-        const fetchpassword = await fetch('http://localhost:5000/api/resetpasswordpage', {
+        const fetchpassword = await fetch('https://employee-crud-prathee.onrender.com/api/resetpasswordpage', {
 
 
             method: 'post',
